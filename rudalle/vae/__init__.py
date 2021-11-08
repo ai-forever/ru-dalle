d5d131b77628fe0690c654907ce1bdd796cd3a1d
shonenkov <shonenkov@phystech.edu>
2021-11-09 00:16:14 +0300

    add dwt checkpoint load

--- rudalle/vae/__init__.py
+++ rudalle/vae/__init__.py
@@ -22,6 +22,9 @@ def get_vae(pretrained=True, dwt=False, cache_dir='/tmp/rudalle'):
         config_file_url = hf_hub_url(repo_id=repo_id, filename=filename)
         cached_download(config_file_url, cache_dir=cache_dir, force_filename=filename)
         checkpoint = torch.load(join(cache_dir, filename), map_location='cpu')
-        vae.model.load_state_dict(checkpoint['state_dict'], strict=False)
+        if dwt:
+            vae.load_state_dict(checkpoint['state_dict'])
+        else:
+            vae.model.load_state_dict(checkpoint['state_dict'], strict=False)
     print('vae --> ready')
     return vae
